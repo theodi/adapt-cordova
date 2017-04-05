9d08256626784474fed720e5dd94c843fad18a8a
Dave Tarrant <davetaz@gmail.com>
2017-04-05 18:40:55 +0100

    First commit on this

---
+++ js/adapt-odi-cordova.js
@@ -0,0 +1,15 @@
+define([
+  'coreJS/adapt',
+], function(Adapt) {
+
+    var cordova = _.extend({
+  
+    	initialize: function() {
+    	},
+
+  	}, Backbone.Events);
+  
+  cordova.initialize();
+
+  return (cordova);
+});
